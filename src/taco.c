b4c7b7cbc8551360e149304c3745836e2bf06e7f
Mariano Dominguez <mariano.dm@gmail.com>
2022-12-03 23:36:49 -0800

    start responding to keyboard

--- src/taco.c
+++ src/taco.c
@@ -4,10 +4,19 @@
 #include <conio.h>
 #include <joystick.h>
 #include <atari.h>
+#include <unistd.h>
+#include <time.h>
+#include <peekpoke.h>
+
+#define KBCODE 0xD209
 
 unsigned char XSize, YSize;
+char tacostr [] = "TACOT";
+char blank [] = "  ";
+char bits []  = "  ";
+unsigned char xcord;
 
-int main_screen(void) {
+void main_screen(void) {
     clrscr ();
     screensize (&XSize, &YSize);
     /* Set screen colors */
@@ -17,19 +26,60 @@ int main_screen(void) {
     
     cvlinexy (XSize-10,2, YSize - 5);
     cvlinexy (XSize-30,2, YSize - 5);
+    xcord=XSize-20;
     chline (21);
+}
+
+void draw_line (int line) {
+    char key;
+    cputsxy(xcord, line, blank);
+
+    if (line==0) {
+        int r = rand() % 4;
+        strncpy(bits, tacostr+r, 2);
+    }
+    cputsxy(xcord, line, bits);
+    key=PEEK(KBCODE);
+    if(key!=255) {
+        //left
+        cputsxy(xcord, line, blank);
+        if(key==KEY_PLUS || key==KEY_A ||  key==KEY_LEFT) {
+            if (xcord>XSize-30) 
+                xcord--;
+        }
+            
+        if(key==KEY_ASTERISK || key==KEY_S || key==KEY_RIGHT) {
+            if (xcord<XSize-10) 
+                xcord++;
+        }
+            
+        cputsxy(xcord, line, bits);
+    }
+    sleep(1);
 }
 
 int main (void)
 {
 
-    int end=0;
+    unsigned int end=0;
+    unsigned int line=0;
+    unsigned long t;
+    time(&t);
+    srand(t);
     main_screen();     
+  
 
-    // while (end=0)
-    // {
-    //     cgetc ();
-    // }
+    while (end==0)
+    {
+
+         draw_line (line);
+         line++;
+         if (line==20) 
+         {
+            line=0;
+         }
+    }
+
     cgetc ();
     return EXIT_SUCCESS;
 }
