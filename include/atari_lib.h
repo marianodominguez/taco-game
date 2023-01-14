bfea291391626071c2250fe203c3d2ce996956f1
Mariano Dominguez <mariano.dm@gmail.com>
2023-01-14 00:27:35 -0800

    sound broken on real atari

--- include/atari_lib.h
+++ include/atari_lib.h
@@ -1,11 +1,5 @@
-
-
-//Temp cpeekc in the meantime it is suported by atari
 #if !defined(__ATARI__)
 #  error This module may only be used when compiling for the Atari!
 #endif
 
-extern void __fastcall__ _cpeekc (void); 
-extern void __fastcall__ grmode (unsigned char mode);  
-extern void __fastcall__ grmode_hack (unsigned char mode); 
-extern void __fastcall__ sound (unsigned char voice, unsigned char pitch, unsigned char distortion, unsigned char volume); 
+extern void __fastcall__ sound (unsigned char voice, unsigned char pitch, unsigned char distortion, unsigned char volume);
